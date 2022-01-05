f489895b651ed94554d0e59444662cc36a88856b
Thomasjuth <thomasjuth@gmail.com>
2022-01-05 17:10:57 +0000

    starting to add javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("let's do this");
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    rockDiv.addEventListener('click', function() {
+        console.log("you, clicked rock");
+    
+    
+    })
+    scissorsDiv.addEventListener('click', function() {
+        window.alert("you, clicked scissors");
+    
+    
+    })
+    paperDiv.addEventListener('click', function() {
+        window.alert("you, clicked paper");
+    
+    
+    })
+
+})
+
+
+const computerScore = 0;
+const scoreBoardDiv = document.querySelector(".board"); 
+const computerScoreSpan = document.getElementById("comp-score");
+const userScoreSpan = document.getElementById("player-score");
+const resultDiv = document.getElementById("result");
+const rockDiv = document.getElementById("r");
+const paperDiv = document.getElementById("p");
+const scissorsDiv = doc.getElementById("s");
+
+
