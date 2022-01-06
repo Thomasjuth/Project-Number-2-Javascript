208ef24103744fecc66e97517fea8862e6256e3d
Thomasjuth <thomasjuth@gmail.com>
2022-01-06 08:21:33 +0000

    adding new photos and more javascrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 
+const computerScore = 0;
+const scoreBoardDiv = document.querySelector(".board"); 
+const computerScoreSpan = document.getElementById("comp-score");
+const userScoreSpan = document.getElementById("player-score");
+const resultDiv = document.getElementById("result");
+const rockDiv = document.getElementById("r");
+const paperDiv = document.getElementById("p");
+const scissorsDiv = document.getElementById("s");
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
+function main () {
 
     rockDiv.addEventListener('click', function() {
-        console.log("you, clicked rock");
+        window.alert("you, clicked rock");
     
     
     })
@@ -19,17 +26,26 @@ document.addEventListener("DOMContentLoaded", function () {
     
     
     })
+    
+    }
 
-})
+function getComputerChoice() {
+
+const options = ["r", "s", "p"];
+const randomNumber = Math.floor(Math.random() * 3);
+return options[randomNumber];
+
+}
 
 
-const computerScore = 0;
-const scoreBoardDiv = document.querySelector(".board"); 
-const computerScoreSpan = document.getElementById("comp-score");
-const userScoreSpan = document.getElementById("player-score");
-const resultDiv = document.getElementById("result");
-const rockDiv = document.getElementById("r");
-const paperDiv = document.getElementById("p");
-const scissorsDiv = doc.getElementById("s");
+function userChoice(){
+
+const computerChoice = getComputerChoice()
+console.log(computerChoice);
+
+}
+
+
+
 
 
